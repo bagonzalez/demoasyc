7bd9d7c0c057df4fe2a224b0250765974480dd9c
Obed <gwilliobed2017@gmail.com>
2022-10-20 22:35:50 -0500

    adding tags

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+       
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+   
       </header>
     </div>
   );
